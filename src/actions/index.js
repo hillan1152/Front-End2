c13ba53b4d0d7f14779dd01884ae121d550e911b
Anthony Hill <hillan1152@gmail.com>
2019-10-21 17:04:34 -0400

    Working on Login, created axiosWithAuth in Utils

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+
+
+const baseURL = 'http://bw-vacaplanning.herokuapp.com';
+
+export const logInUser = (user) => dispatch => {
+    let URL = baseURL + "/login";
+    // dispatch({ type: })
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from "react-redux";
 
-function Login() {
-    return (
-        <div>
+const baseURL = 'http://bw-vacaplanning.herokuapp.com';
+
+
+function Login({ history, token }) {
+    const [user, setUser] = useState({ username: '', password: ''});
+
+    useEffect(() => {
+        if(token) {
+            history.push("/homepage");
+        }
+    }, [history, token])
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(user.username&& user.password){
             
-        </div>
-    )
+        }
+    }
 }
-
-export default Login
