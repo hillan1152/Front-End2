733778d33d0dc17daf9f264e6c5bb49665f938de
Kennethaustin <kenneth.austin93@gmail.com>
2019-10-22 15:56:23 -0400

    commit

---
+++ src/actions/action.js
@@ -0,0 +1,30 @@
+import { newItem } from './src/store/actions/actions';
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+
+class VacaPlanner extends Component {
+
+
+    state = {
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    onSubmitHandle = e => {
+        e.preventDefault(); e.preventDefault();
+        this.props.newItem(this.state)
+    }
+    render() {
+        return (
+            <form onSubmit={this.onSubmitHandle}>
+                <input onChange={this.handleChange} name='name' value={this.state.name} />
+                <input onChange={this.handleChange} name='date' value={this.state.date} />
+                <input onChange={this.handleChange} name='description' value={this.state.description} />
+
+                <button>Submit</button>
+            </form>
+        );
+    }
+}
+export default connect(null, { newItem })(VacaPlanner);
