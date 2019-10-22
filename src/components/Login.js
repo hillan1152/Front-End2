551b6f9f59d68aba6bc17587641c6d3948ad9dc4
Kennethaustin <kenneth.austin93@gmail.com>
2019-10-22 16:10:25 -0400

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { withFormik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import { Button } from '@material-ui/core';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { axioswithAuth } from '../utils/axiosWithAuth';
 
-function Login() {
+const LoginForm = ({ values }) => {
+    const [username, setUsername] = useState('');
     return (
         <div>
-            
+            <Form className='formContainer'>
+                <Field
+                    label='Username'
+                    name='username'
+                    type='text'
+                    variant='filled'
+                    margin='normal'
+                    required
+                    fullWidth
+                    component={TextField}
+                />
+                <Field
+                    label='Password'
+                    name='password'
+                    type='password'
+                    variant='filled'
+                    margin='normal'
+                    required
+                    fullWidth
+                    component={TextField}
+                />
+                <Button variant="contained" color="primary" type='submit'>Login</Button>
+            </Form>
         </div>
     )
 }
 
-export default Login
+const FormikLoginForm = withFormik({
+    mapPropsToValues({ username, password }) {
+        return {
+            username: username || '',
+            password: password || '',
+        }
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required('You need to provide a username to login'),
+        password: Yup.string().required('You need to provide a password to login')
+    }),
+    handleSubmit(values, { resetForm }) {
+        axios({
+            method: 'post',
+            url: 'api-login-goes-here',
+            data: {
+                'username': values.username,
+                'password': values.password
+            }
+        })
+        console.log(values);
+        resetForm();
+    }
+})(LoginForm);
+
+export default FormikLoginForm;
