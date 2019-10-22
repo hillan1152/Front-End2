c12841f581867812ec5256d5dbf3ecb6222ee3ac
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-21 20:31:15 -0400

    Fixed master branch issues

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Comments() {
+const Comments = (props) => {
+    const [post, setPost] = useState({
+        body: '',
+    });
+    const changeHandler = event => {
+        setPost({ ...post, [event.target.name]: event.target.value });
+        console.log(event.target.value);
+    };
+    const submitForm = event => {
+        event.preventDefault();
+        // props.addNewPost(post);
+        setPost({ body: "" });
+    };
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={submitForm}>
+            {/* <input type="text" />
+            <span>😃</span> */}
+            <label htmlFor="body"></label>
+            <textarea
+                name="body"
+                id="body"
+                placeHolder="Type your post here"
+                onChange={changeHandler}
+                value={post.body}
+            />
+            <button type="submit">Post</button>
+        </form>
     )
 }
 
 export default Comments
+
+//need to put <meta charset="UTF-8"> in HTML head if emoji is used
