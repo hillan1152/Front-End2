ed036200230bc9d119c6b27bed91ebe33a4478e0
Kennethaustin <kenneth.austin93@gmail.com>
2019-10-22 12:50:27 -0400

    commit reudcers, axios with auth , privateRoute, actions

---
+++ src/reducers/index.js
@@ -0,0 +1,47 @@
+/*import {
+    FETCH_Item_START,
+    FETCH_Item_SUCCESS,
+    FETCH_Item_FAILURE,
+    ADD_Item_START,
+    ADD_Item_SUCCESS,
+    ADD_Item_FAILURE,
+    EDIT_ITEM_START,
+    EDIT_ITEM_SUCCESS,
+    EDIT_ITEM_FAILURE,
+} from './';
+
+const initialState = {
+    items:[],
+    errors:"",
+    isFetching: false
+}
+
+export const rootReducer = (state = initialState, { type, payload,}) =>{
+    switch (type) {
+        case FETCH_ITEM_START:
+            return {
+                ...state,
+                errors:"",
+                fetching:true
+            };
+        case FETCH_ITEM_SUCCESS:
+            return{
+                ...state,
+                errors:"",
+                items: payload,
+                fetching: false
+            };
+        case FETCH_ITEM_FAILURE:
+            return {
+                ...state,
+                errors: payload,
+                fetching: false
+            };
+        case ADD_ITEM_SUCCESS:
+            return {
+                ...state,
+                fetching:true
+            }    
+
+    }
+}
