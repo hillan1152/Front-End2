72e670de215de01fc1ad6d1d2b16599e85ff0bc0
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-22 18:01:25 -0700

    Define and Rough  Style HomePage.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,67 @@
-
 import React, { Component } from 'react';
 
 import Modal from './Modal/Modal';
-// import ModalTwo from './Modal/ModalTwo';
+import Logo from "../img/Logo.png";
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 100vh;
+    width:100%;
+
+    .top {
+        background: lightgray;
+        padding: 3%;
+        text-align:left;
+        
+            .logoImg {
+                width: 5rem;
+            }//closes logoImg
+
+    }
+
+    .middle {
+        display: flex;
+        
+        
+        .leftSide {
+            padding: 20% 5%;
+            width: 50%;
+            display:flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            h2 {
+                font-size: 2rem;
+            }
+            h3 {
+                font-size: 1rem;
+            }
+
+            .open-modal-btn {
+                margin: 5%;
+                width: 40%;
+                border-radius: 7px;
+                background: #83a931;
+                border: 1px solid black;
+
+                &:hover {
+                    
+                    background: white;
+                border: 1px solid #83a931;
+                }//closes the hover
+            }//closes the open-modal-btn
+        }//closes left
+
+        .rightSide {
+            width: 50%;
+            background: darkgray;
+        }//closes right
+
+    }closes middle
+
+   
+
+`;
 
 class App extends Component {
 
@@ -41,33 +100,46 @@ class App extends Component {
 
     render () {
         return (
-            <section>
-                <div className="left">
-                    <div>
-                        { this.state.isShowingModal1 ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
+            <Container>
+                <header className="top">
+                    <img className="logoImg" src={Logo} alt="Vacation Planner Logo"/>
+                </header>
+                <div className="middle">
+                    <div className="leftSide">
+                        <h2>Vacation Planner</h2>
+                        <h3>Vacation planning made super easy!</h3>
+                        <div>
+                            { this.state.isShowingModal1 ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
 
-                        { this.state.isShowingModal2 ? <div onClick={this.closeModalHandler2} className="back-drop"></div> : null }
+                            { this.state.isShowingModal2 ? <div onClick={this.closeModalHandler2} className="back-drop"></div> : null }
 
-                        <button className="open-modal-btn" onClick={this.openModalHandler}>Sign Up</button>
+                            <button className="open-modal-btn" onClick={this.openModalHandler}>Sign Up</button>
 
-                        <button className="open-modal-btn" onClick={this.openModalHandler2}>Let's Go!</button>
+                            <button className="open-modal-btn" onClick={this.openModalHandler2}>Let's Go!</button>
 
-                        <Modal
-                            className="modal"
-                            show={this.state.isShowingModal1}
-                            close={this.closeModalHandler}>
-                                Sign Up Form
-                        </Modal>
+                            <Modal
+                                className="modal"
+                                show={this.state.isShowingModal1}
+                                close={this.closeModalHandler}>
+                                    Sign Up Form
+                            </Modal>
 
-                        <Modal
-                            className="modal"
-                            show={this.state.isShowingModal2}
-                            close={this.closeModalHandler2}>
-                                Login Form
-                        </Modal>
+                            <Modal
+                                className="modal"
+                                show={this.state.isShowingModal2}
+                                close={this.closeModalHandler2}>
+                                    Login Form
+                            </Modal>
+                        </div>
+                    </div>
+                    <div className="rightSide">
                     </div>
                 </div>
-            </section>
+                <footer className="footsie">
+                    <h3>About</h3>
+                    {/* <a href="">About</a> */}
+                </footer>
+            </Container>
         );
     }
 }
