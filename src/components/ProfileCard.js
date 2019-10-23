e1b252b1891530d6fcd489d939d0389f313ffdac
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-23 07:10:11 -0700

    Create ProfileCard.

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VacationCard = (props) => {
+    return (
+        <div>
+            <h2>{}props.name</h2>
+            <h3>{props.location}</h3>
+            <p>Bio: {props.bio}</p>
+            <p>Ideal Experience: {props.idealExperience}</p>
+        </div>
+    )
+}
+
+export default VacationCard
