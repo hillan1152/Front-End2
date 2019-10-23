82088f9c141ab46b05f7622c12baf0e1227fc184
Kennethaustin <kenneth.austin93@gmail.com>
2019-10-23 17:14:52 -0400

    u

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,38 @@
 import axiosWithAuth from '../utils/axiosWithAuth';
 
-export const AUTHORIZING = "AUTHORIZING";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-
+export const FETCH_ITEM_START = "FETCH_ITEM_START";
+export const FETCH_ITEM_SUCESS = "FETCH_ITEM_SUCCESS";
+export const FETCH_ITEM_FAILURE = "FETCH_ITEM_FAILURE"
 export const ERROR = "ERROR"
 
 const baseURL = 'http://bw-vacaplanning.herokuapp.com';
 
 export const logInUser = (user) => dispatch => {
     let URL = baseURL + "/login";
-    dispatch({ type: AUTHORIZING, payload: "Logging In..."})
+    dispatch({ type: FETCH_ITEM_START, payload: "Logging In..."})
     axiosWithAuth().post(URL, user)
-        .then(res => dispatch({ type: LOGIN_SUCCESS, payload: res.data.token, user: user.username}))
-        .catch(err => dispatch({type: ERROR, payload: err.response.data.code}))
+        .then(res =>{ dispatch({
+            type: FETCH_ITEM_SUCESS,
+            payload: res.data
+        })
+    }, true)
+//            console.log(res.data) )
+        .catch(err => dispatch ({type: FETCH_ITEM_FAILURE,payload: err})) 
+           // console.log(err) ) 
+           // .dispatch({type: ERROR, payload: err.response.data.code})
 }
+
+export const signUpUser = (user) => dispatch => {
+    let URL = baseURL + "/register";
+    dispatch({
+        type: FETCH_ITEM_START})
+        axios
+        .post(URL, user)
+        .then(res =>{
+            console.log(res.data)
+        })
+        .catch(err =>
+            console.log(err)
+             .dispatch({ type:ERROR, payload: err.response.data.code}))
+}
+    
