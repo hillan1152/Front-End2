3af24bbc1419e72c7d2b369db366dda6679af305
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-24 11:36:15 -0700

    Add Footer throughout project Pages.

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -2,6 +2,7 @@ import React from 'react'
 import VacationForm from "./VacationForm";
 import NavBar from "./NavBar";
 import VacationList from "./VacationList";
+import Footer from './Footer';
 
 function Trips(props) {
     return (
@@ -9,6 +10,7 @@ function Trips(props) {
             <NavBar />
             <VacationForm />
             <VacationList />
+            <Footer />
         </div>
     )
 }
