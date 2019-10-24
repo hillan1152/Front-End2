912ff19cae3ece948b7ea5e14a162f03c6446c49
Anthony Hill <hillan1152@gmail.com>
2019-10-23 20:33:51 -0400

    Login into modal

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,6 +4,7 @@ const VacationCard = (props) => {
     return (
         <div>
             <h2>{}props.name</h2>
+            
             {/* <h3>{props.location}</h3>
             <p>Bio: {props.bio}</p>
             <p>Ideal Experience: {props.idealExperience}</p> */}
