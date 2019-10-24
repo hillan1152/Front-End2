b5a89fdd871d9223e99a98493f7b094c82186f13
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-24 11:33:28 -0700

    Style SiteMapPage.

--- src/components/SiteMapPage.js
+++ src/components/SiteMapPage.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from 'react';
+
+import Footer from './Footer';
+import Header from './HeaderFront';
+
+import SMapImg from '../img/siteMap.png';
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 100vh;
+    width:100%;
+
+   .middle {
+    height: 84vh;
+    padding: 5%;
+    background: darkgray;
+    border: 1px solid black;
+
+        h2 {
+            font-size: 4rem;
+            margin: 3%;
+        }
+        img {
+            border: 2px solid black;
+            position: relative;
+            box-shadow: 0 0  10px 5px ;
+        }
+   }
+`;
 
 function SiteMap() {
     return (
-        <div>
-            
-        </div>
+        <Container >
+            <Header />
+                
+                <div className="middle" >
+                    <h2>Site Map</h2>
+                    <img src={SMapImg}  alt="" />
+                </div>
+            <Footer />
+    </Container>
     )
 }
 
