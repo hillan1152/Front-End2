176d3a40439c7dc09cc159bd877d6674ff18889c
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-10-24 12:38:29 -0400

    updating pose

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import pose from "react-pose";
 import Logo from "../../img/Logo.png";
 import './Modal.css';
 
@@ -9,23 +9,23 @@ import styled from 'styled-components';
 
 const modal = (props) => {
     return (
-            <div className="modal-wrapper"
-                style={{
-                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                <div className="modal-header">
-                    <img className="logoImg" src={Logo} alt="Vacation Planner Logo"/>
-                    <h3>Vacation Planner</h3>
-                </div>
-                <div className="modal-body">
-                    {props.children}
-                </div>
-                <div className="modal-footer">
-                    <button className="btn-cancel" onClick={props.close}>Cancel</button>
-                
-                </div>
+        <div className="modal-wrapper"
+            style={{
+                transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            <div className="modal-header">
+                <img className="logoImg" src={Logo} alt="Vacation Planner Logo" />
+                <h3>Vacation Planner</h3>
             </div>
+            <div className="modal-body">
+                {props.children}
+            </div>
+            <div className="modal-footer">
+                <button className="btn-cancel" onClick={props.close}>Cancel</button>
+
+            </div>
+        </div>
     )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,6 @@ const Header = styled.div`
 function NavBar() {
     return (
         <Header>
-           
             <NavModalPage />
         </Header>
     )
